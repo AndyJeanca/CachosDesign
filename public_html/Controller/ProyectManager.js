213c019b4fdcef4e177bd88c3a690f969b257316
Jean Carlo Mata <jean.cms07@gmail.com>
2014-03-29 09:48:53 -0600

    Make the function "paintApixel" in proyect manager
    This function take a point x and point y and make a rectangle 3x3

--- public_html/Controller/ProyectManager.js
+++ public_html/Controller/ProyectManager.js
@@ -4,9 +4,37 @@
  * and open the template in the editor.
  */
 
-function drawInCanvas () {
+function drawAsquare (pX, pY, pWidth, pHeight) {
   var designCanvas = document.getElementById("_Canvas").getContext("2d");
-  designCanvas.fillRect(20, 20, 100, 100);
+  designCanvas.fillRect(pX, pY, pWidth, pHeight);
+};
+
+function drawAline (pXstarting, pYstarting, pXending, pYending) {
+    var designCanvas = document.getElementById("_Canvas").getContext("2d");
+    designCanvas.moveTo(pXstarting, pYstarting);
+    designCanvas.lineTo(pXending, pYending);
+    designCanvas.stroke();
+};
+
+function drawAarc (pX, pY, pRadius, pAngleStarting, pAngleEnding, pAntiClockWise) {
+    var designCanvas = document.getElementById("_Canvas").getContext("2d");
+    designCanvas.beginPath(); 
+    designCanvas.arc(pX, pY, pRadius, pAngleStarting, pAngleEnding, pAntiClockWise); 
+    designCanvas.stroke(); 
+};
+
+function designDefault() {
+    var designCanvas = document.getElementById("_Canvas").getContext("2d");
+    drawAarc(475, 150, 125, 0, 2.9, false); // A-B
+    drawAline(600, 150, 720, 325); // B-C
+    drawAline(720, 325, 880, 390); // C-D
+    drawAarc(165, 250, 200, 0.77, 5.9, true); // A-E
+    drawAline(310, 390, 880, 390);
 };
 
 
+function paintApixel (pX, pY) {  
+    var designCanvas = document.getElementById("_Canvas").getContext("2d");
+    designCanvas.fillRect(pX, pY, 1, 1);
+    designCanvas.stroke(); 
+};
