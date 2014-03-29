9014ce2275241516e99fa6515e388721a6f02e89
Jean Carlo Mata <jean.cms07@gmail.com>
2014-03-28 22:19:52 -0600

    Add canvas and ProyectManager in Controller
    All changes in index

---
+++ public_html/Controller/ProyectManager.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function drawInCanvas () {
+  var designCanvas = document.getElementById("_Canvas").getContext("2d");
+  designCanvas.fillRect(20, 20, 100, 100);
+};
+
+
