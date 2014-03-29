291043e7d5cca6ff15f71d913b8782dfd08f7db8
and951 <andresjimqui@hotmail.com>
2014-03-29 11:01:52 -0600

    Add function drawFromList() and others
    -Making designManager a class
    -adding some functions to designManager();
    etc

--- public_html/Controller/ProyectManager.js
+++ public_html/Controller/ProyectManager.js
@@ -3,27 +3,47 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function ProjectManager(){
+    var actualDesign;
+    
+this.getActualDesign  = function(){
+    return actualDesign;
+};
 
-function drawAsquare (pX, pY, pWidth, pHeight) {
+this.setActualDesign = function(pActualDesign){
+  actualDesign = pActualDesign;  
+};
+
+this.addCircle = function(pLabel, pColor,pBorderSize, pX, pY, pRadius, pFill){
+    actualDesign.addCircle(pLabel, pColor,pBorderSize, pX, pY, pRadius, pFill);
+ };
+ 
+ this.drawFigure=function(pId){
+   var _FigureToDraw = actualDesign.getFigure(pId);
+   var pointsToDraw = _FigureToDraw.drawFigure();
+   drawFromList(pointsToDraw);
+ };
+ 
+this.drawAsquare = function (pX, pY, pWidth, pHeight) {
   var designCanvas = document.getElementById("_Canvas").getContext("2d");
   designCanvas.fillRect(pX, pY, pWidth, pHeight);
 };
 
-function drawAline (pXstarting, pYstarting, pXending, pYending) {
+this.drawAline = function(pXstarting, pYstarting, pXending, pYending) {
     var designCanvas = document.getElementById("_Canvas").getContext("2d");
     designCanvas.moveTo(pXstarting, pYstarting);
     designCanvas.lineTo(pXending, pYending);
     designCanvas.stroke();
 };
 
-function drawAarc (pX, pY, pRadius, pAngleStarting, pAngleEnding, pAntiClockWise) {
+this.drawAarc = function (pX, pY, pRadius, pAngleStarting, pAngleEnding, pAntiClockWise) {
     var designCanvas = document.getElementById("_Canvas").getContext("2d");
     designCanvas.beginPath(); 
     designCanvas.arc(pX, pY, pRadius, pAngleStarting, pAngleEnding, pAntiClockWise); 
     designCanvas.stroke(); 
 };
 
-function designDefault() {
+this.designDefault = function() {
     var designCanvas = document.getElementById("_Canvas").getContext("2d");
     drawAarc(475, 150, 125, 0, 2.9, false); // A-B
     drawAline(600, 150, 720, 325); // B-C
@@ -33,8 +53,29 @@ function designDefault() {
 };
 
 
-function paintApixel (pX, pY) {  
+this.paintApixel= function(pX, pY) {  
     var designCanvas = document.getElementById("_Canvas").getContext("2d");
     designCanvas.fillRect(pX, pY, 1, 1);
     designCanvas.stroke(); 
 };
+
+this.drawFromList=function(pListOfPoints){
+    var ListOfPoints = [];
+    ListOfPoints =pListOfPoints;
+    
+    var amountOfPointsLeft=ListOfPoints.length;
+    
+    for(var positionOfActualPoint = 0; positionOfActualPoint<amountOfPointsLeft;positionOfActualPoint++){
+        var actualPoint = getPointfromListById(positionOfActualPoint,ListOfPoints);
+        paintApixel(actualPoint.getX(),actualPoint.getY());
+    }
+};
+
+this.getPointfromListById = function(pId,pListOfPoints){
+       var ListOfPoints = [];
+       ListOfPoints =pListOfPoints;
+       var arrayWithfigureWithTheId = ListOfPoints.slice(pId,pId);
+       var figureWithTheId = arrayWithfigureWithTheId.pop();
+       return figureWithTheId;
+     };
+}
--- public_html/Model/Design.js
+++ public_html/Model/Design.js
@@ -7,61 +7,62 @@
      var _figures = [];
      var amountOFFigures = 0;
      
-     getDesignName = function(){
+     this.getDesignName = function(){
          return _DesignName;
      };
-     setDesignName = function(pNewDesignName){
+     
+     this.setDesignName = function(pNewDesignName){
          _DesignName = pNewDesignName;
      };
      
-     addCircle = function(pLabel, pColor,pBorderSize, pX, pY, pRadius, pFill){
+     this.addCircle = function(pLabel, pColor,pBorderSize, pX, pY, pRadius, pFill){
          var newCircle = Circle(amountOFFigures,pLabel, pColor, pBorderSize, pX, pY, pRadius, pFill);
          _figures.push(newCircle);
          amountOFFigures++;
          
      };
      
-     addSquare = function (pId, pLabel, pType, pColor, pBorderSize, pX, pY, pWidth, pHeight, pFill){
+     this.addSquare = function (pId, pLabel, pType, pColor, pBorderSize, pX, pY, pWidth, pHeight, pFill){
          var newSquare = Square(pId, pLabel, pType, pColor, pBorderSize, pX, pY, pWidth, pHeight, pFill);
          _figures.push(newSquare);
          amountOFFigures++;  
      };
-     addLine =function (pId, pLabel, pType, pColor, pBorderSize, pX, pY, pX2, pY2){
+     this.addLine =function (pId, pLabel, pType, pColor, pBorderSize, pX, pY, pX2, pY2){
          var newLine = Line(pId, pLabel, pType, pColor, pBorderSize, pX, pY, pX2, pY2);
          _figures.push(newLine);
          amountOFFigures++;  
      };
      
-     getFigure = function(pId){
+     this.getFigure = function(pId){
        var arrayWithfigureWithTheId = _figures.slice(pId,pId);
        var figureWithTheId = arrayWithfigureWithTheId.pop();
        return figureWithTheId;
      };
      
-     modificateConourColor = function(){
+     this.modificateConourColor = function(){
          
          
      };
      
-     modificateSoleColor = function(){
+     this.modificateSoleColor = function(){
          
          
      };
      
-     modificateContourBorderSize = function(){
+     this.modificateContourBorderSize = function(){
          
          
      };
      
-     modificateSoleBorderSize = function(){
+     this.modificateSoleBorderSize = function(){
          
      };
      
-     whatFigureIsinThisPoint=function(pX,pY){
+     this.whatFigureIsinThisPoint=function(pX,pY){
          
      };
      
-     loadDefaultLines = function(){
+     this.loadDefaultLines = function(){
          
      };
      
